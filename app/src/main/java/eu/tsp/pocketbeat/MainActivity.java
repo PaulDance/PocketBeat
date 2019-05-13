030165c3f676a320ff51905cae981388e1a96ce9
Paul Mabileau <paulmabileau@hotmail.fr>
2019-05-13 18:59:43 +0200

    Removed e-mail floating action button
    Also made toolbar buttons active again.
    
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- app/src/main/java/eu/tsp/pocketbeat/MainActivity.java
+++ app/src/main/java/eu/tsp/pocketbeat/MainActivity.java
@@ -5,7 +5,9 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -77,27 +79,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 		});
 		
 		
-		//      ------------        Beyond this point is only automatically generated code          --------------      //
-//		Toolbar toolbar = this.findViewById(R.id.toolbar);
-//		setSupportActionBar(toolbar);
-//
-//		FloatingActionButton fab = this.findViewById(R.id.fab);
-//		fab.setOnClickListener(new View.OnClickListener() {
-//			@Override
-//			public void onClick(View view) {
-//				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//						.setAction("Action", null).show();
-//			}
-//		});
+		final Toolbar toolbar = this.findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
 		
-//		DrawerLayout drawer = this.findViewById(R.id.drawer_layout);
-//		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-//			this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-//		drawer.addDrawerListener(toggle);
-//		toggle.syncState();
+		final DrawerLayout drawer = this.findViewById(R.id.drawer_layout);
+		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+			this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+		drawer.addDrawerListener(toggle);
+		toggle.syncState();
 		
-		NavigationView navigationView = this.findViewById(R.id.nav_view);
-		navigationView.setNavigationItemSelectedListener(this);
+		((NavigationView) this.findViewById(R.id.nav_view)).setNavigationItemSelectedListener(this);
 	}
 	
 	@Override
@@ -132,31 +123,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 		return super.onOptionsItemSelected(item);
 	}
 	
-	@SuppressWarnings("StatementWithEmptyBody")
 	@Override
 	public boolean onNavigationItemSelected(MenuItem item) {        // Handles the left drawer navigation inputs.
 		// Handle navigation view item clicks here.
-		int id = item.getItemId();
-
+		final int id = item.getItemId();
+		final FragmentManager fm = getSupportFragmentManager();
+		
 		if (id == R.id.nav_mic) {
-			FragmentManager fm=getSupportFragmentManager();
 			fm.beginTransaction().replace(R.id.Contentmain, new MicFrag()).commit();
-
 		} else if (id == R.id.nav_mp3) {
-			FragmentManager fm=getSupportFragmentManager();
 			fm.beginTransaction().replace(R.id.Contentmain, new Mp3Frag()).commit();
-
 		} else if (id == R.id.nav_send) {
 
 		} else if (id == R.id.nav_vib) {
-			FragmentManager fm=getSupportFragmentManager();
 			fm.beginTransaction().replace(R.id.Contentmain, new VibFrag()).commit();
 		}
-
+		
 		DrawerLayout drawer = this.findViewById(R.id.drawer_layout);
 		drawer.closeDrawer(GravityCompat.START);
 		return true;
-
-
 	}
 }
