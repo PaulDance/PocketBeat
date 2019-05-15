88f9b50a457165291b5aef5938242a00486605ca
Paul Mabileau <paulmabileau@hotmail.fr>
2019-05-16 00:15:08 +0200

    Rearranged main content to a fragment
    Instead of having the pocket beat elements in the main activity load up in the background and the loaded fragments appear on top of them, they were moved to be used as a fragment also. Layouts were consequently reworked in order to enable the drawer navigator and to make the fragments appear below the toolbar, not under it.
    
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- app/src/main/java/eu/tsp/pocketbeat/MainActivity.java
+++ app/src/main/java/eu/tsp/pocketbeat/MainActivity.java
@@ -1,6 +1,7 @@
 package eu.tsp.pocketbeat;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
@@ -10,15 +11,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.SeekBar;
-import android.widget.TextView;
-import android.widget.ToggleButton;
+
 
 /**
- * MainActivity class for the PocketBeat Android application project. It defines all the activity's interactivity with the
- * application's only layout (for now) and has some components that are not customized but surely will be in the future.
+ * MainActivity class for the PocketBeat Android application project. It defines all the activity's navigation
+ * systems in order for them to be able to load the different components creating this application when needed.
+ *
  * @see BeatWorker
  * @see Settings
  * @author Paul Mabileau
@@ -31,69 +29,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.activity_main);
 		
-		final TextView bpmView = this.findViewById(R.id.bpmView);                           // The text view where the bpm number is displayed.
-		final SeekBar bpmBar = this.findViewById(R.id.bpmBar);                              // The bar used to seek the desired bpm.
-		final BpmSynchronizer bpmSynchronizer = new BpmSynchronizer();                      // The central way to set and get the current bpm.
-		final ButtonBpmGuesser buttonBpmGuesser = new ButtonBpmGuesser(bpmSynchronizer);    // The button beat detector.
-		final BeatWorker beatWorker = new BeatWorker(this, bpmSynchronizer);        // The worker playing the sound in rhythm in the background.
-		
-		bpmBar.setMax(Settings.bpmBarMax - Settings.bpmBarMin);                             // The wanted max of the bar is shifted, to simulate a min.
-		bpmBar.incrementProgressBy(Settings.bpmProgressIncrement);                          // Seems useless for now.
-		
-		bpmSynchronizer.addTextView(bpmView);                                               // Record the different objects
-		bpmSynchronizer.addBar(bpmBar);                                                     // that need synchronization,
-		bpmSynchronizer.setBpm(Settings.bpmInitProgress);                                   // and set them all to have the initial bpm.
-		
-		bpmBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {           // When the bpm seek bar changed,
-			@Override public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-				if (fromUser) {                                                             // and the user did that,
-					buttonBpmGuesser.reset();                                               // reset the beat detector,
-					bpmSynchronizer.setBpm(progress + Settings.bpmBarMin, bpmBar);    // and synchronize the new bpm.
-				}
-			}
-			@Override public void onStartTrackingTouch(SeekBar seekBar) {}
-			@Override public void onStopTrackingTouch(SeekBar seekBar) {}
-		});
-		
-		final ToggleButton toggleBpmButton = this.findViewById(R.id.toggleBpmButton);       // The on/off beat control button.
-		toggleBpmButton.setOnClickListener(new View.OnClickListener() {
-			@Override public void onClick(View view) {                                      // When it is clicked,
-				if (toggleBpmButton.getText().equals("ON")) {                               // and it is 'on',
-					beatWorker.start();                                                     // start the beat,
-				}
-				else {
-					beatWorker.stop();                                                      // otherwise stop it;
-				}
-				buttonBpmGuesser.reset();                                                   // and in any case, reset the detector.
-			}
-		});
-		
-		final Button beatItButton = this.findViewById(R.id.beatItButton);                   // The button that makes the manual beat detection
-		beatItButton.setOnClickListener(buttonBpmGuesser);                                  // gets the associated bpm guesser.
-		
-		final Button resetGuesserButton = this.findViewById(R.id.resetGuesserButton);       // The button used to reset the beat guesser:
-		resetGuesserButton.setOnClickListener(new View.OnClickListener() {
-			@Override public void onClick(View v) {                                         // when clicked,
-				buttonBpmGuesser.reset();                                                   // reset the detector.
-			}
-		});
-		
-		
 		final Toolbar toolbar = this.findViewById(R.id.toolbar);
-		setSupportActionBar(toolbar);
+		this.setSupportActionBar(toolbar);
 		
 		final DrawerLayout drawer = this.findViewById(R.id.drawer_layout);
-		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-			this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
 		drawer.addDrawerListener(toggle);
 		toggle.syncState();
 		
-		((NavigationView) this.findViewById(R.id.nav_view)).setNavigationItemSelectedListener(this);
+		final NavigationView navigationView = this.findViewById(R.id.nav_view);
+		navigationView.getMenu().getItem(0).setChecked(true);
+		navigationView.setNavigationItemSelectedListener(this);
+		this.getSupportFragmentManager().beginTransaction().add(R.id.fragments_container, new VibFrag()).commit();
 	}
 	
 	@Override
 	public void onBackPressed() {
-		DrawerLayout drawer = this.findViewById(R.id.drawer_layout);
+		final DrawerLayout drawer = this.findViewById(R.id.drawer_layout);
+		
 		if (drawer.isDrawerOpen(GravityCompat.START)) {
 			drawer.closeDrawer(GravityCompat.START);
 		} else {
@@ -113,9 +66,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
+		final int id = item.getItemId();
 		
-		//noinspection SimplifiableIfStatement
 		if (id == R.id.action_settings) {
 			return true;
 		}
@@ -124,22 +76,27 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 	}
 	
 	@Override
-	public boolean onNavigationItemSelected(MenuItem item) {        // Handles the left drawer navigation inputs.
-		// Handle navigation view item clicks here.
+	public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+		// Handles the left drawer navigation inputs.
+		System.out.println("ok");
 		final int id = item.getItemId();
-		final FragmentManager fm = getSupportFragmentManager();
+		System.out.println(id);
+		final FragmentManager fm = this.getSupportFragmentManager();
 		
 		if (id == R.id.nav_mic) {
-			fm.beginTransaction().replace(R.id.Contentmain, new MicFrag()).commit();
+			System.out.println("nav_mic");
+			fm.beginTransaction().replace(R.id.fragments_container, new MicFrag()).commit();
 		} else if (id == R.id.nav_mp3) {
-			fm.beginTransaction().replace(R.id.Contentmain, new Mp3Frag()).commit();
+			System.out.println("nav_mp3");
+			fm.beginTransaction().replace(R.id.fragments_container, new Mp3Frag()).commit();
 		} else if (id == R.id.nav_send) {
-
+			System.out.println("nav_send");
 		} else if (id == R.id.nav_vib) {
-			fm.beginTransaction().replace(R.id.Contentmain, new VibFrag()).commit();
+			System.out.println("nav_vib");
+			fm.beginTransaction().replace(R.id.fragments_container, new VibFrag()).commit();
 		}
 		
-		DrawerLayout drawer = this.findViewById(R.id.drawer_layout);
+		final DrawerLayout drawer = this.findViewById(R.id.drawer_layout);
 		drawer.closeDrawer(GravityCompat.START);
 		return true;
 	}
--- app/src/main/java/eu/tsp/pocketbeat/VibFrag.java
+++ app/src/main/java/eu/tsp/pocketbeat/VibFrag.java
@@ -1,17 +1,72 @@
 package eu.tsp.pocketbeat;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class VibFrag extends Fragment {
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View myview=inflater.inflate(R.layout.fragvib, container, false);
-        return myview;
-    }
+	@Nullable
+	@Override
+	public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//System.out.println(container.toString());
+		final View thisView = inflater.inflate(R.layout.fragvib, container, false);
+	
+		final TextView bpmView = thisView.findViewById(R.id.bpmView);						// The text view where the bpm number is displayed.
+		final SeekBar bpmBar = thisView.findViewById(R.id.bpmBar);							// The bar used to seek the desired bpm.
+		final BpmSynchronizer bpmSynchronizer = new BpmSynchronizer();						// The central way to set and get the current bpm.
+		final ButtonBpmGuesser buttonBpmGuesser = new ButtonBpmGuesser(bpmSynchronizer);	// The button beat detector.
+		final BeatWorker beatWorker = new BeatWorker(this.getActivity(), bpmSynchronizer);	// The worker playing the sound in rhythm in the background.
+	
+		bpmBar.setMax(Settings.bpmBarMax - Settings.bpmBarMin);								// The wanted max of the bar is shifted, to simulate a min.
+		bpmBar.incrementProgressBy(Settings.bpmProgressIncrement);							// Seems useless for now.
+	
+		bpmSynchronizer.addTextView(bpmView);												// Record the different objects
+		bpmSynchronizer.addBar(bpmBar);														// that need synchronization,
+		bpmSynchronizer.setBpm(Settings.bpmInitProgress);									// and set them all to have the initial bpm.
+	
+		bpmBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {			// When the bpm seek bar changed,
+			@Override public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+				if (fromUser) {																// and the user did that,
+					buttonBpmGuesser.reset();												// reset the beat detector,
+					bpmSynchronizer.setBpm(progress + Settings.bpmBarMin, bpmBar);		// and synchronize the new bpm.
+				}
+			}
+			@Override public void onStartTrackingTouch(SeekBar seekBar) {}
+			@Override public void onStopTrackingTouch(SeekBar seekBar) {}
+		});
+	
+		final ToggleButton toggleBpmButton = thisView.findViewById(R.id.toggleBpmButton);	// The on/off beat control button.
+		toggleBpmButton.setOnClickListener(new View.OnClickListener() {
+			@Override public void onClick(View view) {										// When it is clicked,
+				if (toggleBpmButton.getText().equals("ON")) {								// and it is 'on',
+					beatWorker.start();														// start the beat,
+				}
+				else {
+					beatWorker.stop();														// otherwise stop it;
+				}
+				buttonBpmGuesser.reset();													// and in any case, reset the detector.
+			}
+		});
+	
+		final Button beatItButton = thisView.findViewById(R.id.beatItButton);				// The button that makes the manual beat detection
+		beatItButton.setOnClickListener(buttonBpmGuesser);									// gets the associated bpm guesser.
+	
+		final Button resetGuesserButton = thisView.findViewById(R.id.resetGuesserButton);	// The button used to reset the beat guesser:
+		resetGuesserButton.setOnClickListener(new View.OnClickListener() {
+			@Override public void onClick(View v) {											// when clicked,
+				buttonBpmGuesser.reset();													// reset the detector.
+			}
+		});
+		
+		return thisView;
+	}
 }
